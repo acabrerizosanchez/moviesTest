12d075856319a53db20f4b51324fabce97da5a25
Alvaro C <toporniz@gmail.com>
2018-02-25 19:33:49 +0100

    @toporniz - #3 - Add movie entity. Include Json serialization. (#4)

--- TestEntities/src/main/java/com/movies/entities/App.java
+++ TestEntities/src/main/java/com/movies/entities/App.java
@@ -1,13 +1,17 @@
 package com.movies.entities;
 
+import java.util.Arrays;
+
 /**
- * Hello world!
+ * Small main to showing the usage of the Entities.
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		String[] genres = { "thriller", "terror" };
+		Movie movie = new Movie("title", "false", "1982", Arrays.asList(genres));
+		System.out.println(movie.toJson());
+		movie = new Movie("title", "false", "1982", "comedy,documentary");
+		System.out.println(movie.toJson());
+	}
 }
